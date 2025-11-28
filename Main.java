0d2f0bce12807d57d76249930dad9fcad8e2cd22
mateus02916-cloud <mateus.02916@aluno.iffar.edu.br>
2025-11-28 20:28:52 -0300

    teste
    teste modicificacao

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ import java.util.*;
 
 
 public class Main {
-    public static public static void main(String[] args) {
+   public static void main (String[] args){
+    Dlfkjalkdjfaçlk
 
-        
-        
-    }
+   }
 
+
+   aslkdjflkajsdçlfkja
     
     
 }
