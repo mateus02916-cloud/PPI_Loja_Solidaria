6a00537dbb83e99d5c91650d24a7dfbd83ead9b6
mateus02916-cloud <mateus.02916@aluno.iffar.edu.br>
2025-12-08 22:11:00 -0300

    teste
    teste

--- Main.java
+++ Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main (String[] args){
         System.out.println("Selecione uma opção:");
 
+        //mudo laskdjfla oasdfl alteraços
+
+        
+
 
     }
     
